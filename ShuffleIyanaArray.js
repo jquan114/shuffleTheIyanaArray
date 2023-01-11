ff8e58f82e9ee5c18446143b44e345055ecb056d
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-10 20:38:12 -0500

    commits at night

---
+++ ShuffleIyanaArray.js
@@ -0,0 +1,15 @@
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+
+var shuffle = function(nums, n) {
+        let newArr = [];
+        
+
+    for( let i = 0 ; i < n ; i++ ) {
+        let x = nums[i];
+        let y = nums[i + n];
+
+        newArr.push(x,y)
+    }
+    return newArr;
+};
